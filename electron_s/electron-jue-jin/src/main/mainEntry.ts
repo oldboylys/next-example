97a7f0c194326c9ab6639cf18d91e53fa1a6020d
lianyanshao <lianyanshao@gmail.com>
2024-08-01 23:49:22 +0800

    initial

---
+++ electron_s/electron-jue-jin/src/main/mainEntry.ts
@@ -0,0 +1,9 @@
+//src\main\mainEntry.ts
+import { app, BrowserWindow } from "electron";
+
+let mainWindow: BrowserWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(process.argv[2]);
+});
---
+++ electron_s/electron-jue-jin/vite.config.ts
@@ -0,0 +1,8 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { devPlugin } from "./plugins/devPlugin";
+
+export default defineConfig({
+  plugins: [devPlugin(), vue()],
+});
